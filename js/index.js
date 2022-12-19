08b170eb75323aecbbfa3f73ec48d69d443e5365
Brandon Perard <Brandon.Perard@Gmail.com>
2022-12-19 15:34:34 -0800

    Added render, local storage, and event listener funcionality

--- js/index.js
+++ js/index.js
@@ -1,14 +1,43 @@
 'use strict';
 
 const diceOptionsArray = [ 6, 12, 20 ];
+const gameObject = {
+  name: 'Player 1',
+  bloodType: 'unknown',
+  diceOption: 6,
+};
+
+// DOM ELEMENTS
 
 const diceOptionsDiv = document.getElementById('diceOptions');
+const nameInput = document.getElementById('nameInput');
+const bloodTypeInput = document.getElementById('bloodTypeInput');
+const readyToRollButton = document.getElementById('readyToRollButton');
 
-// diceOptionsDiv.addEventListener('click', function(event) {
-//   if (event.target.tagName === 'DIV') {
-//     console.log(event.target.innerText);
-//   }
-// });
+// EVENT LISTENERS
+
+diceOptionsDiv.addEventListener('click', function(event) {
+  if (event.target.tagName === 'DIV') {
+    gameObject.diceOption = event.target.innerText;
+  }
+});
+
+nameInput.addEventListener('input', function(event){
+  gameObject.name = event.target.value;
+});
+
+bloodTypeInput.addEventListener('input', function(event) {
+  gameObject.bloodType = event.target.value;
+  console.log(gameObject);
+});
+
+readyToRollButton.addEventListener('click', function() {
+  const stringyGameObject = JSON.stringify(gameObject);
+  localStorage.setItem('gameObject', stringyGameObject);
+  window.location.href = './play.html';
+});
+
+// RUNTIME FUNCTIONS
 
 const buildDiceOptions = function() {
   for (let i = 0; i < diceOptionsArray.length; i++) {
